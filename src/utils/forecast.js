ae7784376683cebb5f189fbfa90960b24dea6af7
Sujal Kodamadanchirayil <sujal23.ks@gmail.com>
2021-12-26 12:52:17 +0800

    Added windspeed data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out.")
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out."+ 'The wind speed is '+ body.current.wind_speed)
         }
     })
 }
